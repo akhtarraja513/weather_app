1b3b9e8235640b5054436f751acd46e7c31ba820
akhtarraja513 <akhtarraja513@gmail.com>
2022-08-28 02:12:28 +0530

    Initial First commit

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "71a8c0db00mshf9f6bb3651cafdfp112e30jsn51202586ecc5",// enter your rapid api key here
+      "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+    },
+  };
+  export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+  
+  export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+  export const WEATHER_API_KEY = "48f9abcdf36cecbe17fd646c8f285ca2"; // enter your key from openweather API
+  
